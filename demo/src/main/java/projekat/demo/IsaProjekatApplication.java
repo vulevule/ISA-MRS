5c21c9b908f513c5374662da9406caf58da509b9
Jovana <jovanaj33@gmail.com>
2018-04-19 18:30:29 +0200

    inicijalni projekat

---
+++ demo/src/main/java/projekat/demo/IsaProjekatApplication.java
@@ -0,0 +1,12 @@
+package projekat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaProjekatApplication.class, args);
+	}
+}
